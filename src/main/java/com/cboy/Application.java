119a6641c17498fd0d83a8a986ddfc8b56c43cff
CharlieYu <yuhangbinccc@gmail.com>
2024-09-07 17:38:48 +0800

    feat: init project

---
+++ src/main/java/com/cboy/Application.java
@@ -0,0 +1,16 @@
+package com.cboy;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
