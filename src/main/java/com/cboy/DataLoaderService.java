343473066306562ab535f52f8a8122b23eab2fae
CharlieYu <yuhangbinccc@gmail.com>
2024-09-12 23:48:05 +0800

    feat: DataLoaderService

---
+++ src/main/java/com/cboy/DataLoaderService.java
@@ -0,0 +1,34 @@
+package com.cboy;
+
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataLoaderService {
+
+    @Value("classpath:/data/xxx.pdf")
+    private Resource resource;
+
+    @Autowired
+    private VectorStore vectorStore;
+
+    public void load() {
+        PagePdfDocumentReader pdfDocumentReader = new PagePdfDocumentReader(this.resource,
+                PdfDocumentReaderConfig.builder()
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.builder()
+                                .withNumberOfBottomTextLinesToDelete(3)
+                                .withNumberOfTopPagesToSkipBeforeDelete(1)
+                                .build())
+                        .withPagesPerDocument(1)
+                        .build());
+        var tokenTextSplitter = new TokenTextSplitter();
+        this.vectorStore.accept(tokenTextSplitter.apply(pdfDocumentReader.get()));
+    }
+}
--- src/main/java/com/cboy/RAGService.java
+++ src/main/java/com/cboy/RAGService.java
@@ -1,6 +1,14 @@
 package com.cboy;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RAGService {
+
+    @Autowired
+    ChatClient client;
 
 
 }
